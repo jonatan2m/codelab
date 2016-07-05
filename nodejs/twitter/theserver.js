c27bd56b827d66df9a39ca162aa703df0430b438
= <=>
2016-07-05 18:32:51 -0300

    twitter part 1 with test

--- nodejs/twitter/theserver.js
+++ nodejs/twitter/theserver.js
@@ -1,9 +1,24 @@
 var express = require('express');
-
+var bodyParser = require('body-parser');
 var app = express();
+app.listen(8000);
+
+var tweets = [];
 
 app.get('/', (req, res) => {
     res.send('Welcome to Node Twitter');
 });
 
-app.listen(8000);
+app.post('/send', bodyParser.urlencoded({ extended: false }), (req, res) => {
+	console.log(req.body);
+	if(req.body && req.body.tweet){
+		tweets.push(req.body.tweet);
+		res.send({status: "ok", message: "Tweet received"});
+	}else{
+		res.send({status: "nok", message: "No tweet received"});
+	}
+});
+
+app.get('/tweets', (req, res) => {
+	res.send(tweets);
+});
