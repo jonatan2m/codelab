a5a8130922b1a6897860dd95b7e8eac8db014189
Jonatan Machado <jonatan.mac@gmail.com>
2017-04-02 14:32:02 -0300

    tour of hanoi in js

---
+++ algorithm/tower-of-hanoi.js
@@ -0,0 +1,42 @@
+function hanoi (n) {
+  var pegs = {
+    A: [], B: [], C: []
+  };
+  
+  function printBoard() {
+    console.log("A>" + pegs.A);
+    console.log("B>" + pegs.B);
+    console.log("C>" + pegs.C);
+    console.log("-----");
+  }
+  
+  function move (disks, from, to) {
+    
+    if(disks === 0) {      
+      //printBoard();
+      return;
+    }
+    else if(disks === 1) {
+      pegs[to].push(pegs[from].pop());
+      //printBoard();
+    }else{      
+      var spared = getSpared(from, to);      
+      move(disks - 1, from, spared);      
+      pegs[to].push(pegs[from].pop());            
+      move(disks - 1, spared, to);
+    }
+  }
+    
+  function getSpared(from, to) {
+    return "ABC".replace(from,"").replace(to,"");
+  }
+  
+  for(var i = n; i > 0; i--){
+        pegs.A.push(i);
+  }
+    
+  move(n, 'A', 'C');
+  printBoard();
+}
+
+hanoi(4)
