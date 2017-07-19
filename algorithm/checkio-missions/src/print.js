fc8c976355a45df9fb95b3b51f0f6f50390d25e8
Jonatan Machado <jonatan.mac@gmail.com>
2017-07-19 13:11:02 -0300

    cleanup dist folder every build

---
+++ algorithm/checkio-missions/src/print.js
@@ -0,0 +1,3 @@
+export default function l(msg) {
+    console.log(`> ${msg}`);
+}
--- algorithm/checkio-missions/webpack.config.js
+++ algorithm/checkio-missions/webpack.config.js
@@ -1,9 +1,17 @@
-var path = require('path');
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        index: './src/index.js',
+        print: './src/print.js'
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist'])
+    ],
     output: {
         filename: 'bundles.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     }
 }
