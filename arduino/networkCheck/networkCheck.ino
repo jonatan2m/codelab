9a56a82d71b974a6f6f7d7f116605fc78cfb2b72
Jonatan Machado <jonatan.mac@gmail.com>
2017-07-19 09:40:58 -0300

    arduino example

--- arduino/networkCheck/networkCheck.ino
+++ arduino/networkCheck/networkCheck.ino
@@ -4,119 +4,85 @@
 
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 EthernetClient client;
-char server[] = "play.avalie.net";
 boolean signal = true;
+const int FAIL_PIN = 2, SUCCESS_PIN = 3;
 
-void Blink1()
+void playAvalie()
 {
-  digitalWrite(13, !digitalRead(13));
+  String url = "GET /HealthCheck/Index?_=";
+  url += random(1, 999999);
+  url += " HTTP/1.1";
+  char server[] = "play.avalie.net";
+
+  if (connectToServer(server, url)) {
+    readMessage();
+  }
 }
 
-void Blink2()
-{
-  //digitalWrite(9, !digitalRead(9));
-  connectToServer();
-  readMessage();
-}
-
-void Blink3()
-{
-  digitalWrite(11, !digitalRead(11));
-}
-
-
-Timer T1(Blink1, 88000, _US_); // 88000us (NOT ms)
-Timer T2(Blink2, 10000);  // 1000ms
-Timer T3(Blink3, 2000); // 2000ms
+//Timer T1(Blink1, 88000, _US_); // 88000us (NOT ms)
+Timer T2(playAvalie, 10000);  // 1000ms
+//Timer T3(Blink3, 2000); // 2000ms
 
 void setup()
 {
   Serial.begin(9600);
   Ethernet.begin(mac);
 
-  delay(1000);
   Serial.println("connecting...");
 
-  //connectToServer();
-  // put your setup code here, to run once:
-  pinMode(8, OUTPUT);
-  pinMode(11, OUTPUT);
-  pinMode(9, OUTPUT);
+  pinMode(SUCCESS_PIN, OUTPUT);
+  pinMode(FAIL_PIN, OUTPUT);
   //T1.start()    ; // optional to use the .start( ) function
-  delay(2568); // On Purpose for you  to compare T1 vs. T2, T3
+  //delay(2568); // On Purpose for you  to compare T1 vs. T2, T3
 }
 
 void loop()
 {
-  /* switch (Ethernet.maintain())
-     {
-     case 1:
-       Serial.println("Error: renewed fail");
-       break;
-     case 2:
-       Serial.println("Renewed success");
-       //print your local IP address:
-       break;
-     case 3:
-       Serial.println("Error: rebind fail");
-       break;
-     case 4:
-       Serial.println("Rebind success");
-       //print your local IP address:
-       break;
-     default:
-       //nothing happened
-       break;
-     }*/
-  if(!signal){
-     digitalWrite(9, !digitalRead(9));
-     delay(1000);
+  if (!signal) {
+    digitalWrite(FAIL_PIN, !digitalRead(FAIL_PIN));
+    delay(1000);
   }
-  // put your main code here, to run repeatedly:
-  //T1.check();
+
   T2.check();
-  T3.check();
 }
 
-void connectToServer() {
+boolean connectToServer(char server[], String url) {
+  stopAllPinStatus();
   client.stop();
-  int result = client.connect("",80);
-  
+  //force clean cache
+  int result = client.connect("", 80);
+
   result = client.connect(server, 80);
-  Serial.println(result);
   if (result) {
     signal = true;
     Serial.println("connected");
-    String url = "GET /HealthCheck/Index?_=";
-    url += random(1, 999999);
-    url += " HTTP/1.1";
+
     client.println(url);
-    client.println("Host: play.avalie.net");
+    String host = "Host: ";
+    host += server;
+    client.println(host);
     client.println("Connection: close");
     client.println();
     while (client.connected() && !client.available()) delay(1); //waits for data
+    return true;
   } else {
     Serial.println("connection failed");
     signal = false;
+    return false;
   }
 }
 void readMessage() {
-  if(!signal) return;
-  
+  if (!signal) return;
+
   String message = "";
   boolean success = false;
-  if (client.available()) {
-    //char c = client.read();
-    //Serial.print(c);
-  }
+
   while (client.available()) {
     char c = client.read();
 
-    if (c == '\n') {
-      if (message.startsWith("HTTP")) {
-        client.stop();
-        break;
-      }
+    if (c == '\n' && message.startsWith("HTTP")) {
+      client.stop();
+      break;
     } else {
       message += c;
     }
@@ -124,13 +90,30 @@ void readMessage() {
   success = message.indexOf("200") > 0;
 
   if (success) {
-    digitalWrite(9, false);
-    digitalWrite(8, true);
+    successRequestPinStatus();
   } else {
-    digitalWrite(9, true);
-    digitalWrite(8, false);
+    failRequestPinStatus();
   }
 
-  Serial.print(message);
+  Serial.println(message);
+}
+
+void successRequestPinStatus() {
+  int pulse = 10;
+  while((pulse--) > 0){
+    digitalWrite(SUCCESS_PIN, !digitalRead(SUCCESS_PIN));
+    delay(100);
+  }
+  digitalWrite(FAIL_PIN, false);
+  //digitalWrite(SUCCESS_PIN, true);
+}
+
+void failRequestPinStatus() {
+  digitalWrite(FAIL_PIN, true);
+  digitalWrite(SUCCESS_PIN, false);
+}
+void stopAllPinStatus() {
+  digitalWrite(FAIL_PIN, false);
+  digitalWrite(SUCCESS_PIN, false);
 }
 
