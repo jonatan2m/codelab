96b37ac0fe6784dbf408efe3e52abb179bcc0838
Jonatan Machado <jonatan.mac@gmail.com>
2017-07-19 13:46:08 -0300

    first unit test example using MochaJS

---
+++ algorithm/checkio-missions/test/example.test.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+
+describe('Example Test', () => {
+    describe('hello world', () => {
+        it('should return "hello world"', () => {
+            assert.equal('hello world', ['h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd'].join(''))
+        })
+    })
+})
