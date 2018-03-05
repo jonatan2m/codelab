3746e020ff079d4374f4df98c219ed02e013b5f6
Jonatan Machado <jonatan.mac@gmail.com>
2018-03-05 16:45:12 -0300

    String Permutation

---
+++ algorithm/recursion/RecursiveLab/RecursiveLab/PermutationString.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecursiveLab
+{
+    internal class PermutationString
+    {
+        internal string[] Permute(string beginningString, string endingString)
+        {
+            if (endingString.Length <= 1)
+                return CombineStringToArray(beginningString, endingString);
+            else
+            {
+                var result = new List<string>();
+                for (int i = 0; i < endingString.Length; i++)
+                {
+                    var newString = RemoveCharAt(endingString, i);
+
+                    result.AddRange(
+                        Permute($"{beginningString}{endingString[i]}", newString)
+                        );
+                }
+
+                return result.ToArray();
+            }
+        }
+
+        private string[] CombineStringToArray(string beginningString, string endingString)
+        {
+            return new string[] { $"{beginningString}{endingString}" };
+        }
+
+        private string RemoveCharAt(string text, int index)
+        {
+            var part1 = text.Substring(0, index);
+            var part2 = text.Substring(index + 1);
+
+            return $"{part1}{part2}";
+        }
+    }
+}
---
+++ algorithm/recursion/RecursiveLab/RecursiveLab/PermutationStringTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RecursiveLab
+{
+    public class PermutationStringTest
+    {
+        [Theory]
+        [InlineData("a", new string[] { "a" })]
+        [InlineData("", new string[] { "" })]
+        [InlineData("ab", new string[] { "ab", "ba" })]
+        [InlineData("abc", new string[] { "abc", "acb", "bac", "bca", "cab", "cba" })]
+        public void Should_return_the_string_permutation(string text, string[] expected)
+        {
+            var permutationString = new PermutationString();
+
+             string[] result = permutationString.Permute("", text);
+
+            Assert.Equal(expected, result);
+
+        }
+    }
+}
