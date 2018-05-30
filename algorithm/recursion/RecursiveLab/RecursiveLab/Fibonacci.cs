0c4bb189f29ea266ae9743503d345a00b97a63ba
Jonatan Machado <jonatan.mac@gmail.com>
2018-05-30 15:41:57 -0300

    add fibonacci function by closed formula

--- algorithm/recursion/RecursiveLab/RecursiveLab/Fibonacci.cs
+++ algorithm/recursion/RecursiveLab/RecursiveLab/Fibonacci.cs
@@ -11,5 +11,14 @@ namespace RecursiveLab
 
             return Calculate(number - 1) + Calculate(number - 2);
         }
+
+        internal double CalculateByClosedFormula(int number)
+        {
+            double part1 = Math.Pow((1 + Math.Sqrt(5)) / 2, number);
+            double part2 = Math.Pow((1 - Math.Sqrt(5)) / 2, number);
+            double fn = (part1 - part2) / Math.Sqrt(5);
+
+            return Math.Round(fn);
+        }
     }
 }
--- algorithm/recursion/RecursiveLab/RecursiveLab/FibonacciTest.cs
+++ algorithm/recursion/RecursiveLab/RecursiveLab/FibonacciTest.cs
@@ -22,5 +22,23 @@ namespace RecursiveLab
 
             Assert.Equal(expected, result);
         }
+
+        [Theory]        
+        [InlineData(1, 1)]
+        [InlineData(2, 1)]
+        [InlineData(3, 2)]
+        [InlineData(4, 3)]
+        [InlineData(5, 5)]
+        [InlineData(6, 8)]
+        [InlineData(7, 13)]
+        [InlineData(20, 6765)]
+        public void Should_return_a_fibo_number_by_closed_formula(int number, int expected)
+        {
+            Fibonacci fb = new Fibonacci();
+
+            var result = fb.CalculateByClosedFormula(number);
+
+            Assert.Equal(expected, result);
+        }
     }
 }
