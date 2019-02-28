3cf3efdec52a3f1a70501c17bfb391f0092e3a94
Jonatan Machado <jonatan.mac@gmail.com>
2019-02-28 11:38:09 -0300

    delegates and events C#

---
+++ dotnetcore/DotNetCoreBootcamp/DelegatesAndEvents/FileLogger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace DelegatesAndEvents
+{
+    public class FileLogger
+    {
+        private readonly string logPath;
+        public FileLogger(string path)
+        {
+            logPath = path;
+            Logger.WriteMessage += Logger_WriteMessage;
+        }
+
+        public void DetachLog() => Logger.WriteMessage -= Logger_WriteMessage;
+
+        void Logger_WriteMessage(string msg)
+        {
+            try
+            {
+                using(var log = File.AppendText(logPath))
+                {
+                    log.WriteLine(msg);
+                    log.Flush();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+    }
+}
---
+++ dotnetcore/DotNetCoreBootcamp/DelegatesAndEvents/Logger.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading;
+
+namespace DelegatesAndEvents
+{
+    public enum Severity
+    {
+        Verbose,
+        Trace,
+        Information,
+        Warning,
+        Error,
+        Critical
+    }
+    public static class Logger
+    {
+        public static Action<string> WriteMessage;
+
+        public static Severity LogLevel { get; set; } = Severity.Warning;
+
+        public static void LogMessage(Severity s, string component, string msg)
+        {
+            if (s < LogLevel) return;
+
+            var output = $"{DateTime.Now}\t{s}\t{component}\t{msg}";
+            WriteMessage?.Invoke(output);
+        }
+
+        //Only to debug
+        static void create()
+        { 
+            var console = new ConsoleLogger();
+            var file = new FileLogger("log.txt");
+        }
+
+        public static void Run()
+        {
+            Logger.LogLevel = Severity.Information;
+
+            create();
+
+            var logLevelRandom = new Random();
+            var logLevels = Enum.GetValues(typeof(Severity));
+
+            Logger.Cancel();
+
+            for (int i = 0; i < 10; i++)
+            {
+               var logLevel = logLevels.GetValue(logLevelRandom.Next(logLevels.Length - 1));
+               Logger.LogMessage((Severity)logLevel, "Main", "Meu teste maroto");
+               //GC.Collect();
+            }
+
+        }
+
+        private static void Cancel()
+        {
+            WriteMessage = null;
+        }
+    }
+}
