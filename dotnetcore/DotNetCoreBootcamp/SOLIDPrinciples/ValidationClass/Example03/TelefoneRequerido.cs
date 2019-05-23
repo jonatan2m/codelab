a801bfa6161ebd539b0899c7227ef22eb4206822
Jonatan Machado <jonatan.mac@gmail.com>
2019-05-23 09:05:12 -0300

    examples validation class

---
+++ dotnetcore/DotNetCoreBootcamp/SOLIDPrinciples/ValidationClass/Example03/TelefoneRequerido.cs
@@ -0,0 +1,19 @@
+﻿namespace SOLIDPrinciples.ValidationClass.Example03
+{
+    public class TelefoneRequerido : IRegraDeValidacao<Cliente>
+    {
+        IRegraDeValidacao<Cliente> proximaRegra;
+        public TelefoneRequerido(IRegraDeValidacao<Cliente> proximaRegra)
+        {
+            this.proximaRegra = proximaRegra;
+        }
+        public TelefoneRequerido() { }
+
+        public bool ehValido(Cliente cliente)
+        {
+            //regra de validacao de telefone.
+            //Se tiver outras regras, chama o atributo passado no construtor
+            return true;
+        }
+    }
+}
