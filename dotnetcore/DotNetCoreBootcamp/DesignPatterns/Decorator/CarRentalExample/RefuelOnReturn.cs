af0b6f52edd1980b9810a2b533fffd60745a3d62
Jonatan Machado <jonatan.mac@gmail.com>
2019-07-14 13:00:15 -0300

    WIP Decorator

---
+++ dotnetcore/DotNetCoreBootcamp/DesignPatterns/Decorator/CarRentalExample/RefuelOnReturn.cs
@@ -0,0 +1,22 @@
+﻿namespace DesignPatterns.Decorator.CarRentalExample
+{
+    public class RefuelOnReturn : CarRentalDecorator
+    {
+        private readonly float _refuelPrice;
+
+        public RefuelOnReturn(IRental rental, float refuelPrice)
+        :base(rental)
+        {
+            _refuelPrice = refuelPrice;
+        }
+        public override float CalcPrice()
+        {
+            return rental.CalcPrice() + RefuelPrice();
+        }
+
+        private float RefuelPrice()
+        {
+            return (GetModel().fuelCapacity - GetFuelConsumed()) * _refuelPrice;
+        }
+    }
+}
