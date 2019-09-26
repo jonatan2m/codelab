b9f074403872c0cbae58719aaf8fb2f9f77a73ee
Jonatan Machado <jonatan.mac@gmail.com>
2019-09-26 07:27:01 -0300

    some examples of factory

---
+++ dotnetcore/DotNetCoreBootcamp/DesignPatterns/Factory/LoanExample/RiskFactor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPatterns.Factory.LoanExample
+{
+    internal class RiskFactor
+    {
+        internal static double ForRiskRating(int rating)
+        {
+            return rating;
+        }
+    }
+}
--- dotnetcore/DotNetCoreBootcamp/DesignPatternsTest/FactoryTest.cs
+++ dotnetcore/DotNetCoreBootcamp/DesignPatternsTest/FactoryTest.cs
@@ -1,5 +1,7 @@
 ﻿using DesignPatterns.Factory.AbstractFactory;
+using DesignPatterns.Factory.EncapsulateClasses;
 using DesignPatterns.Factory.Example1;
+using DesignPatterns.Factory.LoanExample;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -21,7 +23,41 @@ namespace DesignPatternsTest
         {
             PizzaStore pizzaStore = new NYPizzaStore();
             pizzaStore.OrderPizza("cheese");
-            Assert.IsType<House>(house);
+            Assert.IsAssignableFrom<PizzaStore>(pizzaStore);
+        }
+
+        private readonly int RISK_RATING = 1;
+
+        [Fact]
+        public void Loan_Factory_Example()
+        {
+            //arrange
+            DateTime maturity = DateTime.Now.AddDays(5);
+            DateTime? startOfLoan = DateTime.Now.AddYears(-2);
+
+            Loan termLoan = LoanFactory.newTermLoan(10000.00, startOfLoan, maturity, RISK_RATING);
+            termLoan.setOutstanding(10000.00);
+
+            //act
+            var result = termLoan.calcCapital();
+
+            //assert
+            Assert.Equal(20136, (int)result);
+        }
+
+        [Fact]
+        public void EncapsulateClasses_AttributeDescriptor()
+        {
+            //arrange
+            var result = new List<AttributeDescriptor>();
+
+            //Act
+            result.Add(AttributeDescriptor.ForInt("remoteId"));
+            result.Add(AttributeDescriptor.ForDatetime("createdDate"));
+            result.Add(AttributeDescriptor.ForBool("isBuyer"));
+
+            //assert
+            Assert.Equal("remoteId", result[0].GetName());
         }
     }
 }
