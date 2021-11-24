76f7b9f6dc269d724e56658891b3c679c8ddd7bf
Jonatan Machado <jonatan.mac@gmail.com>
2021-11-24 06:25:51 -0300

    type code with state/strategy

--- dotnetcore/DotNetCoreBootcamp/Refactoring/ReplaceConditionalWithPolymorphism.cs
+++ dotnetcore/DotNetCoreBootcamp/Refactoring/ReplaceConditionalWithPolymorphism.cs
@@ -5,55 +5,109 @@ using Xunit;
 
 namespace Refactoring
 {
-    public class ReplaceConditionalWithPolymorphism
-    {
-        [Fact]
-        public void ReplaceConditionalWithPolymorphismBefore()
-        {
-
-        }
-
-    }
-
-    public abstract class EmployeeType
-    {
-        public abstract int GetTypeCode();
-    }
-
-    public class Engineer : EmployeeType
-    {
-        public override int GetTypeCode()
-        {
-            throw new NotImplementedException();
-        }
-    }
-
+    //Replace Type Code with State/Strategy
     class Employee
     {
-        private EmployeeType _type;
         private int _monthlySalary;
         private int _commission;
         private int _bonus;
 
-        public Employee(EmployeeType type)
+        private EmployeeType _type;
+
+        Employee(int type)
         {
-            _type = type;
+            setType(type);
         }
 
-        public int PayAmount()
+        //step one - encapsulate the type
+        int getType()
         {
-            return 0;
-            //switch (_type.GetTypeCode())
-            //{
-            //    case EmployeeType.ENGINEER:
-            //        return _monthlySalary;
-            //    case EmployeeType.SALESMAN:
-            //        return _monthlySalary + _commission;
-            //    case EmployeeType.MANAGER:
-            //        return _monthlySalary + _bonus;
-            //    default:
-            //        throw new ArgumentOutOfRangeException("Incorrect Employee");
-            //}
+            return _type.getTypeCode();
+        }
+
+        void setType(int arg)
+        {
+            _type = EmployeeType.newType(arg);            
+        }
+
+        int payAmount()
+        {
+            switch (getType())
+            {
+                case EmployeeType.ENGINEER:
+                    return _monthlySalary;
+                case EmployeeType.SALESMAN:
+                    return _monthlySalary + _commission;
+                case EmployeeType.MANAGER:
+                    return _monthlySalary + _bonus;
+                default:
+                    throw new ArgumentException("Incorrect Employee");
+            }
         }
     }
-}
+
+    //step two - declare abstract class and a method for returning the type code
+    abstract class EmployeeType
+    {
+        //step four - move all knowlegde about codes and subclasses over to the EmployeeType class
+        public const int ENGINEER = 0;
+        public const int SALESMAN = 1;
+        public const int MANAGER = 2;
+
+        public abstract int getTypeCode();
+
+        //step five - Bring the Employee.setType method statement to this class and create a such of factory method.
+        public static EmployeeType newType(int code)
+        {
+            switch (code)
+            {
+                case ENGINEER:
+                    return new Engineer();
+                case SALESMAN:
+                    return new Salesman();
+                case MANAGER:
+                    return new Manager();
+                default:
+                    throw new ArgumentException("Incorrect Employee Code");
+            }
+        }
+
+        //step three - create subclasses
+        class Engineer : EmployeeType
+        {
+            public override int getTypeCode()
+            {
+                return Employee.ENGINEER;
+            }
+        }
+
+        //step three - create subclasses
+        class Manager : EmployeeType
+        {
+            public override int getTypeCode()
+            {
+                return Employee.MANAGER;
+            }
+        }
+
+        //step three - create subclasses
+        class Salesman : EmployeeType
+        {
+            public override int getTypeCode()
+            {
+                return Employee.SALESMAN;
+            }
+        }
+
+
+        public class ReplaceConditionalWithPolymorphism
+        {
+            [Fact]
+            public void ReplaceConditionalWithPolymorphismBefore()
+            {
+
+            }
+
+        }
+
+    }
