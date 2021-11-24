fa400feffec0b2e6b3bdd5db37cf0fdf7f013a59
Jonatan Machado <jonatan.mac@gmail.com>
2021-11-24 05:13:26 -0300

    add interface segregation example

---
+++ dotnetcore/DotNetCoreBootcamp/Refactoring/InterfaceSegregation.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Refactoring
+{
+    public class InterfaceSegregation
+    {
+        [Fact]
+        public void Before()
+        {
+            var before = new Segregation_Before();
+
+            UsingMMakeSomeOne(before);
+        }
+
+        [Fact]
+        public void After()
+        {
+            var after = new Segregation_After();
+
+            UsingMMakeSomeOne(after);
+        }
+
+        private void UsingMMakeSomeOne(Segregation_Before segregation)
+        {
+            //the is method only need the MakeSomething_One and the other Segregation_Before fields aren't needed.
+            segregation.MakeSomething_One();
+        }
+        private void UsingMMakeSomeOne(Interface_One one)
+        {
+            //the is method only need the MakeSomething_One and the other Segregation_Before fields aren't needed.
+            one.MakeSomething_One();
+        }
+    }
+
+    public class Segregation_Before
+    {
+        public void MakeSomething_One() { Console.WriteLine("One"); }
+        public void MakeSomething_Two() { Console.WriteLine("Two"); }
+    }
+
+    public class Segregation_After : Interface_One, Interface_Two
+    {
+        public void MakeSomething_One() { Console.WriteLine("One"); }
+        public void MakeSomething_Two() { Console.WriteLine("Two"); }
+    }
+
+    public interface Interface_One
+    {
+        void MakeSomething_One();
+    }
+
+    public interface Interface_Two
+    {
+        void MakeSomething_Two();
+    }
+
+    
+   
+}
---
+++ dotnetcore/DotNetCoreBootcamp/Refactoring/ReplaceConditionalWithPolymorphism.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Refactoring
+{
+    public class ReplaceConditionalWithPolymorphism
+    {
+        [Fact]
+        public void ReplaceConditionalWithPolymorphismBefore()
+        {
+
+        }
+
+    }
+
+    public abstract class EmployeeType
+    {
+        public abstract int GetTypeCode();
+    }
+
+    public class Engineer : EmployeeType
+    {
+        public override int GetTypeCode()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Employee
+    {
+        private EmployeeType _type;
+        private int _monthlySalary;
+        private int _commission;
+        private int _bonus;
+
+        public Employee(EmployeeType type)
+        {
+            _type = type;
+        }
+
+        public int PayAmount()
+        {
+            return 0;
+            //switch (_type.GetTypeCode())
+            //{
+            //    case EmployeeType.ENGINEER:
+            //        return _monthlySalary;
+            //    case EmployeeType.SALESMAN:
+            //        return _monthlySalary + _commission;
+            //    case EmployeeType.MANAGER:
+            //        return _monthlySalary + _bonus;
+            //    default:
+            //        throw new ArgumentOutOfRangeException("Incorrect Employee");
+            //}
+        }
+    }
+}
