43b7aa37261da992eb60602cd1eb838fedee0596
Jonatan Machado <jonatan.mac@gmail.com>
2022-01-19 11:02:00 -0300

    using INotifyPropertyChanged to control property modifications

---
+++ dotnetcore/DotNetCoreBootcamp/DomainEventsTest/NotifyPropertyChangedExample/StudentTest.cs
@@ -0,0 +1,27 @@
+﻿using DomainEvents.NotifyPropertyChangedExample;
+using System;
+using Xunit;
+
+namespace DomainEventsTest.NotifyPropertyChangedExample
+{
+    public class StudentTest
+    {
+        [Fact]
+        public void CreateStudentAndChangeGradeProperty()
+        {
+            var sut = new Student("John", 123456);
+
+            sut.PropertyChanged += (obj, arg) => Console.WriteLine(arg.PropertyName); ;
+
+            sut.Grade = 10;
+        }
+
+        [Fact]
+        public void CreateStudentAndVerifyIfGradePropertyIsChange()
+        {
+            var sut = new Student("John", 123456);
+
+            Assert.PropertyChanged(sut, nameof(sut.Grade), () => sut.UpdateGrade(10));
+        }        
+    }
+}
