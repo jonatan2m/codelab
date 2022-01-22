07a99c7c5deafa58164bb1b9bf4b6ac29c7b4ff7
Jonatan Machado <jonatan.mac@gmail.com>
2022-01-22 13:49:29 -0300

    xunit sharing context

---
+++ dotnetcore/DotNetCoreBootcamp/DomainEventsTest/SharingContext/GameStateFixture.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xunit;
+
+namespace DomainEventsTest.SharingContext
+{
+    public class GameStateFixture : IDisposable
+    {
+        public readonly GameState GameState;
+
+        public GameStateFixture()
+        {
+            GameState = new GameState();
+        }
+
+        public void Dispose()
+        {
+            //Cleanup
+        }
+    }
+}
---
+++ dotnetcore/DotNetCoreBootcamp/DomainEventsTest/SharingContext/Scenarios.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DomainEventsTest.SharingContext
+{
+    public class Player
+    {
+        public readonly string Name;
+        public int Health { get; private set; }
+
+        public Player(string name)
+        {
+            Name = name;
+            Health = new Random().Next(50, 100);
+        }
+
+        public void TakeDamage(int damage)
+        {
+            Health -= damage;
+        }
+    }
+
+    public class GameState
+    {
+        public IList<Player> Players { get; set; } = new List<Player>();
+
+        public Guid Id { get; } = Guid.NewGuid();
+
+        public const int EarthquakeDamage = 25;
+
+        public GameState()
+        {
+            CreateNewWorld();
+        }
+
+        public void Reset()
+        {
+            Players.Clear();
+        }
+
+        public void Earthquake()
+        {
+            foreach (var player in Players)
+            {
+                player.TakeDamage(EarthquakeDamage);
+            }
+        }
+
+        private void CreateNewWorld()
+        {
+            //expensive creation
+            System.Threading.Thread.Sleep(2000);
+        }
+    }
+}
