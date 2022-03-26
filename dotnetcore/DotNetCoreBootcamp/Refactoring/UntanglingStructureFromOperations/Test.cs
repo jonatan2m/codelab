6c7d2049397d9b6355e59a49cc4361ac4df4e105
Jonatan Machado <jonatan.mac@gmail.com>
2022-03-26 19:23:34 -0300

    adding examples of composite pattern

---
+++ dotnetcore/DotNetCoreBootcamp/Refactoring/UntanglingStructureFromOperations/Test.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace Refactoring.UntanglingStructureFromOperations
+{
+    public class Test
+    {
+        [Fact]
+        public void Just_instantiate_the_painter_group()
+        {
+            IEnumerable<ProportionalPainter> painters = new ProportionalPainter[10];
+
+            IPainter painter = CompositePainterFactories.CreateGroup(painters);
+        }
+    }
+}
