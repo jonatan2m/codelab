9084da5f2ed16dc654c98a857b436ce8363642d7
Jonatan Machado <jonatan.mac@gmail.com>
2022-12-22 11:45:24 -0300

    snapshot testing

---
+++ dotnetcore/DotNetCoreBootcamp/GeneralResources/XUnitExample/SnapshotTesting/ClassToBeTested.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneralResources.XUnitExample.SnapshotTesting
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Age { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Guid InternalCode { get; set; }
+    }
+    public class ClassToBeTested
+    {
+        public static Person GetPerson()
+        {
+            return new()
+            {
+                Id = 2,
+                Name = "Test",
+                Description = "Test",
+                Age = 41,
+                BirthDate = new DateTime(2022, 12, 22),
+                InternalCode = Guid.NewGuid()
+            };
+        }
+    }
+
+    [UsesVerify]
+    public class SnapshotTest
+    {
+        [Fact]
+        public async Task GetPersonTest()
+        {
+            //https://github.com/VerifyTests/Verify/blob/main/docs/serializer-settings.md
+            var settings = new VerifySettings();
+            settings.DontScrubDateTimes();            
+
+            var person = ClassToBeTested.GetPerson();
+
+            await Verify(person, settings);
+        }
+    }
+}
---
+++ dotnetcore/DotNetCoreBootcamp/GeneralResources/XUnitExample/SnapshotTesting/README.md
@@ -0,0 +1,13 @@
+﻿Snapshot testing is a way to validate complex data models and documents.
+It generate a file, with the name following the test's name. On the next execution, the tool will compare the lasted file against the current file.
+If everything is ok, the test passes, otherwise, it'll fail.
+
+The first result will be versioned and all other execution must to fit with the previous result.
+
+Here is a reference to this test: https://github.com/VerifyTests/Verify
+Youtube video that I discovered it https://www.youtube.com/watch?v=Q1_YkcPwpqY
+
+Snapshot tests never assert the correct behavior of the application functionality but does an output comparison instead.
+
+- https://jestjs.io/docs/snapshot-testing
+- https://circleci.com/blog/snapshot-testing-with-jest/#:~:text=Snapshot%20testing%20is%20a%20type,values%20of%20your%20development%20team.
