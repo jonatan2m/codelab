11b33aef8b070ebbc18fd2336c2f84a40c2f6705
Jonatan Machado <jonatan.mac@gmail.com>
2022-12-22 09:42:34 -0300

    start bloom filter example

---
+++ dotnetcore/DotNetCoreBootcamp/GeneralResources/CursoAlgoritmoEstruturaDeDados/Algoritmos/BloomFilter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneralResources.CursoAlgoritmoEstruturaDeDados.Algoritmos
+{
+    public class BloomFilter
+    {
+        [Fact]
+        public void T()
+        {
+            var bits = new BitArray(5);
+            bits[0] = true;
+        }
+    }
+}
