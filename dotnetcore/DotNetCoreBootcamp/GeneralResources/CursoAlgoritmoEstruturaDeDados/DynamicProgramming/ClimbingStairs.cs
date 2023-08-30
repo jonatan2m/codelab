9f86dfea8adb081f8714ddca23b76523bdd1e1b0
Jonatan Machado <jonatan.mac@gmail.com>
2023-08-30 10:45:41 -0300

    other examples of dynamic programming

--- dotnetcore/DotNetCoreBootcamp/GeneralResources/CursoAlgoritmoEstruturaDeDados/DynamicProgramming/ClimbingStairs.cs
+++ dotnetcore/DotNetCoreBootcamp/GeneralResources/CursoAlgoritmoEstruturaDeDados/DynamicProgramming/ClimbingStairs.cs
@@ -21,6 +21,7 @@ namespace GeneralResources.CursoAlgoritmoEstruturaDeDados.DynamicProgramming
 
         //Return 0, if impossible to get the top and 1 if it is possible
         //O(n)
+        //Top-Down Dynamic Programming (It starts from the )
         public int CalculeTheWaysToClimbRecursiveAndMemoization(int totalSteps, Dictionary<int, int>? memo = null, int currentSteps = 0)
         {
             memo ??= new Dictionary<int, int>();
@@ -40,10 +41,21 @@ namespace GeneralResources.CursoAlgoritmoEstruturaDeDados.DynamicProgramming
         }
 
         //https://www.youtube.com/watch?v=Y0lT9Fck7qI
-        //Bottom-up
-        public int CalculeWithDifferentApproach()
+        //Bottom-up dynamic programming (improved)
+        //It starts from the base case. In this problem, the base case is the top of the stairs.
+        public int CalculeWithDifferentApproach(int totalSteps)
         {
-            return 0;
+            int a = 1;
+            int b = 1;
+            int c = 0;
+            for (int i = totalSteps - 2; i >= 0; i--)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+            }
+
+            return c;
         }
 
         [Theory]
@@ -75,6 +87,23 @@ namespace GeneralResources.CursoAlgoritmoEstruturaDeDados.DynamicProgramming
 
             //Act
             var result = sut.CalculeTheWaysToClimbRecursiveAndMemoization(input);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(2, 2)]
+        [InlineData(3, 3)]
+        [InlineData(5, 8)]
+        [InlineData(15, 987)]
+        public void SelfTest3(int input, int expected)
+        {
+            //Arrange
+            var sut = new ClimbingStairs();
+
+            //Act
+            var result = sut.CalculeWithDifferentApproach(input);
 
             //Assert
             Assert.Equal(expected, result);
