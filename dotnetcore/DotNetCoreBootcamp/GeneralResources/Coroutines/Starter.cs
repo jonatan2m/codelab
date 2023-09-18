da165b1318431c8b45f48cb82f356df7c6bb6c5e
Jonatan Machado <jonatan.mac@gmail.com>
2023-09-18 10:12:35 -0300

    first example of coroutine

--- dotnetcore/DotNetCoreBootcamp/GeneralResources/Coroutines/Starter.cs
+++ dotnetcore/DotNetCoreBootcamp/GeneralResources/Coroutines/Starter.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,5 +13,23 @@ namespace GeneralResources.Coroutines
     /// </summary>
     public class Starter
     {
+        IEnumerable<Task> rotina()
+        {
+            Console.WriteLine("inicio do fluxo");
+            yield return Task.Delay(30000);
+            Console.WriteLine("passou-se 3 segundos");
+            yield return Task.Delay(2000);
+            Console.WriteLine("passou-se 2 segundos");
+            yield return Task.CompletedTask;
+        }
+
+        [Fact]
+        public async void Teste1()
+        {
+            foreach (var item in rotina())
+            {
+                await item;
+            }
+        }
     }
 }
